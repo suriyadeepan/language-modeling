969b84110acddaa93b3e82a77b4950702b4e79f0
Suriyadeepan Ramamoorthy <suriyadeepan.r@gmail.com>
2017-01-06 23:33:37 +0530

    update code to read config from ini file

--- generate.py
+++ generate.py
@@ -1,30 +1,38 @@
-import sms_data as data
+import data
+import utils
 import char_rnn
 
+import sys
+
 if __name__ == '__main__':
-    # fetch metadata dictionaries
-    X, Y, idx2ch, ch2idx = data.load_data()
+    if len(sys.argv) > 3:
+        # get configuration
+        g = utils.get_config(sys.argv[1])
+        # fetch metadata dictionaries
+        X, Y, idx2ch, ch2idx = data.load_data(path=g['data_path'])
 
-    # build the model
-    num_classes = len(idx2ch)
-    net = char_rnn.CharRNN(seqlen= 1,
-                           num_classes= num_classes,
-                           num_layers= 2,
-                           state_size= 128,
-                           epochs= 0,
-                           learning_rate= 0.1,
-                           batch_size= 1, 
-                           ckpt_path= 'ckpt/',
-                           model_name= 'sms_lm')
+        # build the model
+        num_classes = len(idx2ch)
+        net = char_rnn.CharRNN(seqlen= 1,
+                               num_classes= num_classes,
+                               num_layers= g['num_layers'],
+                               state_size= g['state_size'],
+                               epochs= 0,
+                               learning_rate= 0.1,
+                               batch_size= 1, 
+                               ckpt_path= g['ckpt_path'],
+                               model_name= g['model_name']
+                               )
 
-    # generate 100 characters for each char in our vocabulary
-    #  and let's write it to a file!
-    with open('data/sms_results.txt', 'w') as f:
-        for ch in idx2ch:
-            char_indices = net.generate_characters(
-                    num_chars= 100, init_char_idx= ch2idx[ch])
-            msg = ''.join([ idx2ch[chidx] for chidx in char_indices ])
-            print('________________________________________________')
-            print(msg)
-            f.write('\n' + msg + '\n')
+        char_indices = net.generate_characters(
+                num_chars= int(sys.argv[3]), init_char_idx= ch2idx[sys.argv[2]])
+        msg = ''.join([ idx2ch[chidx] for chidx in char_indices ])
+        print(msg)
 
+    else:
+        print('\n>> Usage')
+        print(':: python3 generate.py config_file.ini <s> <n>')
+        print('\t<s> : starting character')
+        print('\t<n> : number of characters to generate')
+        print('>> example')
+        print(':: python3 generate.py tf_src_code.ini d 1000')
--- train.py
+++ train.py
@@ -1,30 +1,51 @@
-PATH = 'data/madurai/'
-#PATH = 'data/sms/'
-
 import data
 import utils
 import char_rnn
 
+import sys
+
+
 if __name__ == '__main__':
-    # fetch dataset
-    X, Y, idx2ch, ch2idx = data.load_data(path=PATH)
+    # get cmd line args from user <- config file
+    if len(sys.argv) > 1:
+        # get configuration
+        g = utils.get_config(sys.argv[1])
+        # check if we need to process text file
+        if g['data_file'] and utils.isEmpty(g['ckpt_path']):
+            # check if folder exists, if not create folder
+            utils.assert_dir(g['data_path'])
+            data.process_data(filename= g['data_file'],
+                    path= g['data_path'])
+        # check if checkpoint path exists
+        utils.assert_dir(g['ckpt_path'])
 
-    batch_size = 256
+        try:
+            # fetch dataset
+            X, Y, idx2ch, ch2idx = data.load_data(path=g['data_path'])
+        except:
+            print('\n>> Is the folder {} empty?'.format(g['ckpt_path']))
+            print('Shouldn\'t it be?')
+            sys.exit()
 
-    # training set batch generator
-    trainset = utils.rand_batch_gen(X, Y, batch_size= batch_size)
+        # training set batch generator
+        trainset = utils.rand_batch_gen(X, Y, batch_size= g['batch_size'])
 
-    # build the model
-    num_classes = len(idx2ch)
-    net = char_rnn.CharRNN(seqlen= X.shape[-1], 
-                           num_classes= num_classes,
-                           num_layers= 4,
-                           state_size= 256,
-                           epochs= 1000000,
-                           learning_rate= 0.1,
-                           batch_size= batch_size,
-                           ckpt_path= 'ckpt/madurai/',
-                           model_name= 'madurai_lm')
+        # build the model
+        num_classes = len(idx2ch)
+        net = char_rnn.CharRNN(seqlen= X.shape[-1], 
+                               num_classes= num_classes,
+                               num_layers= g['num_layers'],
+                               state_size= g['state_size'],
+                               epochs= 100000000,
+                               learning_rate= g['learning_rate'],
+                               batch_size= g['batch_size'],
+                               ckpt_path= g['ckpt_path'],
+                               model_name= g['model_name']
+                               )
 
-    # train on trainset
-    net.train(trainset)
+        # train on trainset
+        net.train(trainset)
+
+    else:
+        print('\nUsage')
+        print(':: python3 train.py config_file.ini')
